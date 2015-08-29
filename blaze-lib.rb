b012d3931dd84c772f2bdfe14376bb887c63c524
Hendrik Gossler <hendrik.gossler@kit.edu>
2015-08-29 17:17:49 -0600

    blaze-lib 2.4 (new formula)
    Open-source, high-performance C++ math library for dense and sparse arithmetic

--- blaze-lib.rb
+++ blaze-lib.rb
@@ -1,5 +1,5 @@
 class BlazeLib < Formula
-  desc "High-performance C++ math library for dense and sparse arithmetic"
+  desc "C++ math library for dense and sparse arithmetic"
   homepage "https://bitbucket.org/blaze-lib/blaze/"
   url "https://bitbucket.org/blaze-lib/blaze/downloads/blaze-2.4.tar.gz"
   sha256 "34af70c8bb4da5fd0017b7c47e5efbfef9aadbabc5aae416582901a4059d1fa3"
@@ -8,6 +8,11 @@ class BlazeLib < Formula
 
   def install
     inreplace "Configfile", "CXX=\"g++\"", "CXX=\"#{ENV.cxx}\""
+
+    # Set compiler flags for intel C++ compiler
+    if ENV.cxx.include? "icpc"
+      inreplace "Configfile", "CXXFLAGS=\"-Werror -Wall -Wextra -Wshadow -Woverloaded-virtual -ansi -O3 -DNDEBUG\"", "CXXFLAGS=\"-Werror -Wshadow -w1 -ansi -O3 -DNDEBUG\""
+    end
 
     system "./configure"
     system "make"
@@ -19,9 +24,25 @@ class BlazeLib < Formula
   test do
     (testpath/"test.cpp").write <<-EOS.undent
       #include <blaze/Blaze.h>
+
+      using blaze::StaticVector;
+      using blaze::DynamicVector;
+
       int main(int argc, char *argv[])
       {
-        return 0;
+        // Instantiation of a static 3D column vector. The vector is directly initialized as
+        StaticVector<int,3UL> a( 4, -2, 5 );
+
+        // Instantiation of a dynamic 3D column vector. Via the subscript operator the values are set to
+        DynamicVector<int> b( 3UL );
+        b[0] = 2;
+        b[1] = 5;
+        b[2] = -3;
+
+        // Adding the vectors a and b
+        DynamicVector<int> c = a + b;
+
+        return (c[0] == 6 && c[1] == 3 && c[2] == 2) ? 0 : -1;
       }
     EOS
 
